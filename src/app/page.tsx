c3235589ba7ba728fcc746bf28782064cb7e209c
Weyne <augustoweyne12@gmail.com>
2025-10-06 19:44:51 -0300

    Chore: removido componente nãu usado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-type Props = {
-  title: string;
-};
-
-const Component = ({ title }: Props) => {
-  console.log(title);
-  return (
-    <>
-      <h2>Compoenente</h2>
-    </>
-  );
-};
-
 export default function Home() {
   return (
     <>
       <h1>HOME</h1>
-      <Component title={1} />
     </>
   );
 }

34b8485353d24a64cc6eb55fcc9b549afb5c4417
Weyne <augustoweyne12@gmail.com>
2025-10-06 19:40:57 -0300

    feature: Instalado PNPM e lefthook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+type Props = {
+  title: string;
+};
+
+const Component = ({ title }: Props) => {
+  console.log(title);
+  return (
+    <>
+      <h2>Compoenente</h2>
+    </>
+  );
+};
+
 export default function Home() {
   return (
     <>
       <h1>HOME</h1>
+      <Component title={1} />
     </>
   );
 }

866d1da564a6babb582be65b6ceafb926d2f6ec3
Weyne Siqueira <augustoweyne12@gmail.com>
2025-10-08 18:02:10 -0300

    feature: criado formulario e criado os inputs

--- src/components/AppointmentForm/appointment-form.tsx
+++ src/components/AppointmentForm/appointment-form.tsx
@@ -9,19 +9,160 @@ import {
   DialogDescription,
 } from '../ui/dialog';
 import { Button } from '../ui/button';
+import z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+import { Dog, Phone, User } from 'lucide-react';
+import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
+import { IMaskInput } from 'react-imask';
+
+const appointmentsFormSchema = z.object({
+  tutorName: z.string().min(3, 'O nome do tutor é obrigatorio e maior que 3!'),
+  petName: z.string().min(3, 'O nome do pet é Obrigatorio e maior que 3!'),
+  phone: z.string().min(11, 'O telefone é obrigatorio'),
+  description: z.string().min(3, 'A descrição é obrigatorio'),
+});
+
+type AppointmentFormValues = z.infer<typeof appointmentsFormSchema>;
 
 export const AppointmentForm = () => {
+  const form = useForm<AppointmentFormValues>({
+    resolver: zodResolver(appointmentsFormSchema),
+    defaultValues: {
+      description: '',
+      tutorName: '',
+      petName: '',
+      phone: '',
+    },
+  });
+
+  const onSubmit = (data: AppointmentFormValues) => {};
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="brand">Novo Agendamento</Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent
+        variant="appointment"
+        overlayVariant="blurred"
+        showCloseButton
+      >
         <DialogHeader>
-          <DialogTitle>Agende um atendimento</DialogTitle>
-          <DialogDescription>
+          <DialogTitle size="modal">Agende um atendimento</DialogTitle>
+          <DialogDescription size="modal">
             Preencha os dados do cliente para realizar o agendamento:
           </DialogDescription>
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="tutorName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-label-medium-size text-content-primary">
+                      Nome do Tutor
+                    </FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <User
+                          className="absolute left-3 top-1/2 -translate-y-1/2 transform text-content-brand"
+                          size={20}
+                        />
+                        <Input
+                          placeholder="Nome do Tutor"
+                          className="pl-10"
+                          {...field}
+                        />
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="petName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-label-medium-size text-content-primary">
+                      Nome do Pet
+                    </FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Dog
+                          className="absolute left-3 top-1/2 -translate-y-1/2 transform text-content-brand"
+                          size={20}
+                        />
+                        <Input
+                          placeholder="Nome do Pet"
+                          className="pl-10"
+                          {...field}
+                        />
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-label-medium-size text-content-primary">
+                      Telefone
+                    </FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Phone
+                          className="absolute left-3 top-1/2 -translate-y-1/2 transform text-content-brand"
+                          size={20}
+                        />
+                        <IMaskInput
+                          placeholder="(99) 99999-9999"
+                          mask="(00) 00000-0000"
+                          className="pl-10 flex h-12 w-full rounded-md border border-border-primary bg-background-tertiary px-3 py-2 text-sm text-content-primary ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-content-secondary focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-0 focus-visible:ring-border-brand disabled:cursor-not-allowed disabled:opacity-50 hover:border-border-secondary focus:border-border-brand focus-visible:border-border-brand aria-invalid:ring-destructive/20 aria-invalid:border-destructive"
+                          {...field}
+                        />
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-label-medium-size text-content-primary">
+                      Descrição do serviço
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Descrição do serviço"
+                        className="resize-none"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <button type="submit">Salvar</button>
+            </form>
+          </Form>
         </DialogHeader>
       </DialogContent>
     </Dialog>
